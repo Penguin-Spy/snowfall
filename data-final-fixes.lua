9789ead9381cb0d074a9e650d0992d22d6b6b63a
Penguin_Spy <me@penguinspy.dev>
2024-11-10 17:13:34 -0800

    temporary fix for alien biomes settings

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,3 +9,49 @@ for _, character in pairs(data.raw.character) do
   table.insert(character.crafting_categories, "wire-drawing")
 end
 ]]
+
+-- help alien biomes finish removing vegetation
+if settings.startup["alien-biomes-disable-vegetation"].value == "Enabled" then
+  --for _, prototype in pairs(data.raw['tree']) do
+  --  prototype.autoplace = nil
+  --end
+
+  local autoplace_tile_settings_to_remove = {}
+  for _, prototype in pairs(data.raw['tile']) do
+    if string.find(prototype.name, "grass") then
+      --prototype.autoplace = nil
+      table.insert(autoplace_tile_settings_to_remove, prototype.name)
+    end
+  end
+
+  local autoplace_decorative_settings_to_remove = {}
+  local block_decorative_words = {"grass", "asterisk", "fluff", "garballo", "bush", "croton", "pita", "cane"}
+  for _, prototype in pairs(data.raw['optimized-decorative']) do
+    for _, word in pairs(block_decorative_words) do
+      if string.find(prototype.name, word) then
+        --prototype.autoplace = nil
+        table.insert(autoplace_decorative_settings_to_remove, prototype.name)
+      end
+    end
+  end
+
+  for _, planet in pairs(data.raw["planet"]) do
+    local mgsas = planet.map_gen_settings.autoplace_settings
+    if mgsas then
+      if mgsas.decorative then
+        for _, name in pairs(autoplace_decorative_settings_to_remove) do
+          mgsas.decorative.settings[name] = nil
+        end
+      end
+      if mgsas.tile then
+        for _, name in pairs(autoplace_tile_settings_to_remove) do
+          mgsas.tile.settings[name] = nil
+        end
+      end
+      if mgsas.entity then
+        mgsas.entity.settings['fish'] = nil
+      end
+    end
+  end
+  --data.raw['fish']['fish'].autoplace = nil
+end
--- settings.lua
+++ settings.lua
@@ -11,7 +11,3 @@ data.raw["string-setting"]["alien-biomes-disable-vegetation"].default_value = "E
 data.raw["string-setting"]["alien-biomes-include-rivers"].hidden = true
 data.raw["string-setting"]["alien-biomes-include-rivers"].allowed_values = { "Enabled" }
 data.raw["string-setting"]["alien-biomes-include-rivers"].default_value = "Enabled"
-
---[[data.raw["bool-setting"]["enable-upscaled-hr-resolution"].hidden = true
-data.raw["bool-setting"]["enable-upscaled-hr-resolution"].default_value = false
-data.raw["bool-setting"]["enable-upscaled-hr-resolution"].forced_value = false]]
